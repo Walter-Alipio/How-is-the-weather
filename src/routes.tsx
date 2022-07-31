0a2311ccf64e4e798236080f079b2d9c228b7b0b
Walter <silvawalteralipio@gmail.com>
2022-07-31 15:59:30 -0300

    routes for home and weather

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import AccessPage from './components/Access';
 import Home from './pages/home';
+import Weather from './pages/weather';
 
 export default function AppRoutes(){
+  const [city,setCity] = useState('');
   return (
     <BrowserRouter>
-    <main className='h-screen flex items-center justify-center'>
+    
       <Routes>
-        <Route path='/' element={<Home />}>
-          <Route index element={ <Home />} />
+        <Route path='/' element={<AccessPage />}>
+          <Route index element={ <Home setCity={setCity} />} />
+          <Route path='clima' element={<Weather city={city}/>} />
         </Route>
       </Routes>
-    </main>
+ 
     </BrowserRouter>
   )
 }
