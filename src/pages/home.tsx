3bafc3fc5c7892fdb352a6ea5312024cdca4cb29
Walter <silvawalteralipio@gmail.com>
2022-07-31 15:58:24 -0300

    home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,29 @@
+import { useNavigate }  from 'react-router-dom';
 
-export default function Home(){
+interface Props {
+  setCity: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Home({ setCity }: Props){
+  const navigate = useNavigate();
+
+  function handleEvent(event: React.ChangeEvent<HTMLInputElement>){
+    event.preventDefault;
+    setCity(event.target.value);
+    navigate('clima')
+  }
+
   return(
-     <h1 className="text-red-600 font-bold">Hello World!</h1>
+    <section className="flex flex-col items-center gap-5 max-w-[44375rem]">
+      <h1 className="text-white font-bold text-3xl text-center">Como está o tempo hoje?</h1>
+      <input type="text" 
+        name="city" 
+        id="city"
+        aria-label='Nome da cidade' 
+        placeholder="Digite o nome da cidade"
+        onBlur={(event)=>{handleEvent(event)}}
+        className="w-full h-12 rounded-xl p-4 placeholder:text-placeholder text-placeholder"/>  
+
+    </section>
   )
 }
