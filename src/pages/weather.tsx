5d53885c059a3d1b9b9abc7787b00b174b4455d8
Walter <silvawalteralipio@gmail.com>
2022-08-01 17:00:38 -0300

    getting data from api

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -1,21 +1,56 @@
+import { useEffect, useState } from "react";
+import { ICoordinate, IUnit } from "../routes";
 
 interface Props {
   city: string;
+  unit: IUnit;
+  coordinate: ICoordinate;
 }
 
-export default function Weather({ city }: Props){
+export default function Weather({ city, unit, coordinate }: Props){
+
   
-  const weather = 'Nuvens dispersas';
-  const temp = 29;
-  const maxTemp = 29;
-  const minTemp = 14;
+  const [weather,setWeather] = useState<string>('');
+  const [temp,setTemp] = useState<number>(0);
+  const [maxTemp,setMaxTemp] = useState<number>(0);
+  const [minTemp,setMinTemp] = useState<number>(0);
+  const [icon, setIcon] = useState('')
+
+  const units = unit.unit
+  const API_KEY = import.meta.env.VITE_WEATHER_KEY;
+  
+  const url =`http://api.openweathermap.org/data/3.0/onecall?lat=${coordinate.lat}&lon=${coordinate.lng}&units=${units}&appid=${API_KEY}&lang=pt_br`
+  
+  console.log(units)
+  useEffect(()=>{
+    const getWeather =async () => {
+      const res = await fetch(url).then(data => data.json()).catch(err=>console.log(err));
+
+      console.log(res);
+      setWeather(res.current.weather[0].description);
+      setTemp(parseInt(res.current.temp));
+      setMaxTemp(parseInt(res.daily[0].temp.max));
+      setMinTemp(parseInt(res.daily[0].temp.min));
+      setIcon(res.current.weather[0].icon);
+      console.log(new Date(res.daily[1].dt))
+    }
+    getWeather();
+  },[unit])
 
   return (
-    <section className="flex flex-col items-center gap-1 max-w-[44375rem] text-white">
-      <h1 className="font-bold text-3xl text-center uppercase">{city}</h1>
-      <span className="text-sm">{weather}</span>
-      <span className="text-5xl">{temp}°</span>
-      <div className="flex text-xl gap-2">
+    <section className="flex flex-col items-center gap-1 max-w-[44.375rem] md:min-w-[40rem] text-white">
+      <h1 className="font-bold text-3xl text-center uppercase md:text-5xl">{city}</h1>
+      <span className="text-sm first-letter:uppercase text-center md:text-xl">{weather}</span>
+      <div className="flex items-center">
+        <span className="text-5xl md:text-6xl">{temp}°</span>
+        {icon !== '' ? 
+          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" 
+          
+            className="w-14 h-14 md:w-24 md:h-24"
+          />
+          : ''}
+      </div>
+      <div className="flex text-xl gap-2 md:text-xl">
         <span className="uppercase font-bold">Max:</span>
         <span>{maxTemp}°</span>
         <span className="uppercase font-bold">Min:</span>
